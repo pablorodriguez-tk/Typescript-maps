afeb85cc242d9c5e357f560c7ada54544b2a83b7
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-04-21 18:55:55 -0300

    Add aleatory user mark on map

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { User } from "./User";
-import { Company } from "./Company";
-
+// import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 const user = new User();
+const customMap = new CustomMap("map");
 
-console.log(user);
-
-const company = new Company();
-
-console.log(company);
+customMap.addUserMarker(user);
